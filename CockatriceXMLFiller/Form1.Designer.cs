4a0a1854f570524a15a6eba927bc3481b040e6bc
Alexandre Homem <alexman657@gmail.com>
2024-05-20 16:45:45 +0100

    Add project files.

---
+++ CockatriceXMLFiller/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace CockatriceXMLFiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxCardname = new System.Windows.Forms.TextBox();
+            this.labelCardname = new System.Windows.Forms.Label();
+            this.labelSet = new System.Windows.Forms.Label();
+            this.textBoxSet = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxType = new System.Windows.Forms.TextBox();
+            this.labelManacost = new System.Windows.Forms.Label();
+            this.textBoxManacost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPower = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxToughness = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxIdentity = new System.Windows.Forms.TextBox();
+            this.panelCard = new System.Windows.Forms.Panel();
+            this.panelStart = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panelCard.SuspendLayout();
+            this.panelStart.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Honeydew;
+            this.button1.Location = new System.Drawing.Point(6, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate card";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rubik", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(61, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cockatrice card register";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Honeydew;
+            this.button2.Location = new System.Drawing.Point(17, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxCardname
+            // 
+            this.textBoxCardname.Location = new System.Drawing.Point(3, 27);
+            this.textBoxCardname.Name = "textBoxCardname";
+            this.textBoxCardname.Size = new System.Drawing.Size(162, 20);
+            this.textBoxCardname.TabIndex = 4;
+            // 
+            // labelCardname
+            // 
+            this.labelCardname.AutoSize = true;
+            this.labelCardname.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCardname.Location = new System.Drawing.Point(3, 10);
+            this.labelCardname.Name = "labelCardname";
+            this.labelCardname.Size = new System.Drawing.Size(73, 15);
+            this.labelCardname.TabIndex = 5;
+            this.labelCardname.Text = "Card name";
+            // 
+            // labelSet
+            // 
+            this.labelSet.AutoSize = true;
+            this.labelSet.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSet.Location = new System.Drawing.Point(3, 49);
+            this.labelSet.Name = "labelSet";
+            this.labelSet.Size = new System.Drawing.Size(27, 15);
+            this.labelSet.TabIndex = 7;
+            this.labelSet.Text = "Set";
+            // 
+            // textBoxSet
+            // 
+            this.textBoxSet.Location = new System.Drawing.Point(3, 66);
+            this.textBoxSet.Name = "textBoxSet";
+            this.textBoxSet.Size = new System.Drawing.Size(162, 20);
+            this.textBoxSet.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Type";
+            // 
+            // textBoxType
+            // 
+            this.textBoxType.Location = new System.Drawing.Point(3, 105);
+            this.textBoxType.Name = "textBoxType";
+            this.textBoxType.Size = new System.Drawing.Size(162, 20);
+            this.textBoxType.TabIndex = 8;
+            // 
+            // labelManacost
+            // 
+            this.labelManacost.AutoSize = true;
+            this.labelManacost.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelManacost.Location = new System.Drawing.Point(3, 128);
+            this.labelManacost.Name = "labelManacost";
+            this.labelManacost.Size = new System.Drawing.Size(69, 15);
+            this.labelManacost.TabIndex = 11;
+            this.labelManacost.Text = "Mana cost";
+            // 
+            // textBoxManacost
+            // 
+            this.textBoxManacost.Location = new System.Drawing.Point(3, 145);
+            this.textBoxManacost.Name = "textBoxManacost";
+            this.textBoxManacost.Size = new System.Drawing.Size(162, 20);
+            this.textBoxManacost.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Power";
+            // 
+            // textBoxPower
+            // 
+            this.textBoxPower.Location = new System.Drawing.Point(3, 248);
+            this.textBoxPower.Name = "textBoxPower";
+            this.textBoxPower.Size = new System.Drawing.Size(45, 20);
+            this.textBoxPower.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(54, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "/";
+            // 
+            // textBoxToughness
+            // 
+            this.textBoxToughness.Location = new System.Drawing.Point(73, 248);
+            this.textBoxToughness.Name = "textBoxToughness";
+            this.textBoxToughness.Size = new System.Drawing.Size(45, 20);
+            this.textBoxToughness.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(70, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 15);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Toughness";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Card identity";
+            // 
+            // textBoxIdentity
+            // 
+            this.textBoxIdentity.Location = new System.Drawing.Point(3, 195);
+            this.textBoxIdentity.Name = "textBoxIdentity";
+            this.textBoxIdentity.Size = new System.Drawing.Size(162, 20);
+            this.textBoxIdentity.TabIndex = 17;
+            // 
+            // panelCard
+            // 
+            this.panelCard.Controls.Add(this.labelCardname);
+            this.panelCard.Controls.Add(this.label7);
+            this.panelCard.Controls.Add(this.button1);
+            this.panelCard.Controls.Add(this.textBoxIdentity);
+            this.panelCard.Controls.Add(this.textBoxCardname);
+            this.panelCard.Controls.Add(this.label6);
+            this.panelCard.Controls.Add(this.textBoxSet);
+            this.panelCard.Controls.Add(this.textBoxToughness);
+            this.panelCard.Controls.Add(this.labelSet);
+            this.panelCard.Controls.Add(this.label4);
+            this.panelCard.Controls.Add(this.textBoxType);
+            this.panelCard.Controls.Add(this.label3);
+            this.panelCard.Controls.Add(this.label5);
+            this.panelCard.Controls.Add(this.textBoxPower);
+            this.panelCard.Controls.Add(this.textBoxManacost);
+            this.panelCard.Controls.Add(this.labelManacost);
+            this.panelCard.Location = new System.Drawing.Point(12, 58);
+            this.panelCard.Name = "panelCard";
+            this.panelCard.Size = new System.Drawing.Size(200, 437);
+            this.panelCard.TabIndex = 19;
+            this.panelCard.Visible = false;
+            // 
+            // panelStart
+            // 
+            this.panelStart.Controls.Add(this.button2);
+            this.panelStart.Location = new System.Drawing.Point(95, 67);
+            this.panelStart.Name = "panelStart";
+            this.panelStart.Size = new System.Drawing.Size(200, 75);
+            this.panelStart.TabIndex = 19;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Honeydew;
+            this.button3.Location = new System.Drawing.Point(12, 518);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 37);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 636);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panelStart);
+            this.Controls.Add(this.panelCard);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Cockatrice card register";
+            this.panelCard.ResumeLayout(false);
+            this.panelCard.PerformLayout();
+            this.panelStart.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxCardname;
+        private System.Windows.Forms.Label labelCardname;
+        private System.Windows.Forms.Label labelSet;
+        private System.Windows.Forms.TextBox textBoxSet;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxType;
+        private System.Windows.Forms.Label labelManacost;
+        private System.Windows.Forms.TextBox textBoxManacost;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxToughness;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxPower;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxIdentity;
+        private System.Windows.Forms.Panel panelCard;
+        private System.Windows.Forms.Panel panelStart;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CockatriceXMLFiller/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CockatriceXMLFiller
+{
+    public partial class Form1 : Form
+    {
+
+
+        public string name;
+        public string set;
+        public string type;
+        public string maintype;
+        public string manacost;
+        public string cmc;
+        public string colors;
+        public string coloridentity;
+        public static int power;
+        public static int toughness;
+        private string pt = power.ToString() + "/" + toughness.ToString();
+
+
+        public Form1()
+        {
+
+        InitializeComponent();
+
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            Application.Exit();
+        
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+
+            name = textBoxCardname.Text;
+            set = textBoxSet.Text;
+            type = textBoxType.Text;
+            
+            switch(type)
+            {
+                case string creatures when type.Contains("Creature") : maintype = "Creature";
+                    break;
+                case string artifacts when type.Contains("Artifact"): maintype = "Artifact";
+                    break;
+                case string enchantments when type.Contains("Enchantment"): maintype = "Enchantment";
+                    break;
+                case string lands when type.Contains("Lands"): maintype = "Land";
+                    break;
+
+            }
+
+            manacost = textBoxManacost.Text;
+            colors = Regex.Replace(manacost, @"[\d-]", string.Empty);
+            power = Int32.Parse(textBoxPower.Text); 
+            toughness = Int32.Parse(textBoxToughness.Text);
+            coloridentity = textBoxIdentity.Text;
+
+
+
+                
+
+
+            using (XmlWriter writer = XmlWriter.Create(name + ".xml"))
+            {
+                writer.WriteStartElement("card");
+                writer.WriteElementString("name", name);
+                writer.WriteElementString("set", set);
+                writer.WriteStartElement("prop");
+                writer.WriteElementString("type", type);
+                writer.WriteElementString("maintype", maintype);
+                writer.WriteElementString("manacost", manacost);
+                writer.WriteElementString("cmc", cmc);
+                writer.WriteElementString("colors", colors);
+                writer.WriteElementString("coloridentity", coloridentity);
+                writer.WriteElementString("pt", pt);
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.Flush();
+            }
+
+
+            MessageBox.Show("Card XML generated successfully");
+            DialogResult dialogResult = MessageBox.Show("Would you like to open it now?", "", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start(@"C:\Users\Utilizador\source\repos\CockatriceXMLFiller\CockatriceXMLFiller\bin\Debug\" + name + ".xml");
+
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do nothing
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panelCard.Visible = true;
+            panelStart.Visible = false;
+            label2.Visible = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
